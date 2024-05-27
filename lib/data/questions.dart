618c84ce9c332ba512a28b09ea0ccc4d709dc86c
Ilham Resky Maulana <vascoprofessional@gmail.com>
2024-05-27 21:32:28 +0700

    test

---
+++ lib/data/questions.dart
@@ -0,0 +1,94 @@
+import 'package:adv_basics/models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion(
+    'Penduduk yang tinggal di daerah tepi pantai dapat memanfaatkan sumber daya alam berupa ....',
+    [
+      'air',
+      'ikan',
+      'tanah',
+      'batu bara',
+    ],
+  ),
+  QuizQuestion(
+    'Salah satu manfaat dari minyak bumi bagi kehidupan manusia adalah untuk ....',
+    [
+      'bahan bakar kendaraan',
+      'sebagai aksesoris',
+      'bahan kosmetik',
+      'bahan bangunan',
+    ],
+  ),
+  QuizQuestion(
+    'Dalam pemanfaatan sumber daya alam, manusia haruslah bijak memanfaatkan sumber daya alam yang ada, hal ini dilakukan agar kelestarian sumber daya alam tersebut dapat terjaga. Usaha yang dapat dilakukan manusia agar dapat menjaga kelestarian sumber daya alam adalah ....',
+    [
+      'penghentian penggunaan sumber daya alam',
+      'mengimpor sumber daya alam dari luar negeri',
+      'mencari sumber daya alam alternatif',
+      'menggunakan sumber daya alam sesukanya',
+    ],
+  ),
+  QuizQuestion(
+    'Makna dari koperasi konsumsi adalah ...',
+    [
+      'koperasi yang meminjamkan modal kepada para anggotanya',
+      'koperasi yang menyediakan kebutuhan pokok para anggotanya',
+      'koperasi yang membantu usaha para anggotanya',
+      'koperasi yang jumlah anggotanya 100 orang',
+    ],
+  ),
+  QuizQuestion(
+    'Suku bangsa yang berasal dari Pulau Jawa adalah',
+    [
+      'Sunda, Tengger, Betawi, Madura',
+      'Betawi, Badui, Lampung, Bugis',
+      'Melayu, Badui, Padang, Toba',
+      'Jawa, Dayak, Karo, Banjar',
+    ],
+  ),
+  QuizQuestion(
+    'Keragaman suku bangsa di Indonesia merupakan kenyataan yang tidak dapat dipungkiri. Kita harus menghargai setiap perbedaan yang ada. Perbedaan suku-suku bangsa itu dipengaruhi oleh ...',
+    [
+      'banyaknya gunung berapi',
+      'persamaan lingkungan pulau yang didiami',
+      'perbedaan kondisi lingkungan yang ditempati',
+      'Indonesia negara yang ditakuti oleh negara lain di dunia',
+    ],
+  ),
+  QuizQuestion(
+    'Alat musik sasando berasal dari daerah',
+    [
+      'Papua',
+      'NTT',
+      'Bali',
+      'NTB',
+    ],
+  ),
+  QuizQuestion(
+    'Pakaian adat suku Batak Toba merupakan kain tenun yang sering dijadikan sebagai ciri khas suku Batak. Bahkan menjadi identitas dari pakaian adat Sumatera Utara secara nasional Berdasarkan pernyataan tersebut, nama pakaian adatnya adalah ….',
+    [
+      'beskap',
+      'kebaya',
+      'ulos',
+      'baju bodo',
+    ],
+  ),
+  QuizQuestion(
+    'Keragaman budaya yang ada di Indonesia tercermin dari …',
+    [
+      'kesenian daerah, fanatisme daerah, senjata tradisional',
+      'bahasa daerah, pakaian adat, fanatisme daerah',
+      'fanatisme daerah, kesenian daerah, bahasa daerah',
+      'pakaian adat, kesenian daerah, bahasa daerah',
+    ],
+  ),
+  QuizQuestion(
+    'Raja Makasar yang mendapat julukan sebagai Ayam Jantan Dari Timur adalah …',
+    [
+      'Sultan Agung',
+      'Sultan Ageng Tirtayasa',
+      'Sultan Zainal Abidin',
+      'Sultan Hasannudin',
+    ],
+  ),
+];
---
+++ lib/quiz.dart
@@ -0,0 +1,77 @@
+import 'package:adv_basics/data/questions.dart';
+import 'package:adv_basics/questions_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:adv_basics/start_screen.dart';
+import 'package:adv_basics/results_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  List<String> selectedAnswers = [];
+  var activeScreen = 'start-screen';
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = 'questions-screen';
+    });
+  }
+
+  void chooseAnswer(String answer) {
+    selectedAnswers.add(answer);
+
+    if (selectedAnswers.length == questions.length) {
+      setState(() {
+        activeScreen = 'result-screen';
+      });
+    }
+  }
+
+  void restartQuiz() {
+    setState(() {
+      selectedAnswers = [];
+      activeScreen = 'questions_screen';
+    });
+  }
+
+  @override
+  Widget build(context) {
+    Widget screenWidget = StartScreen(switchScreen);
+
+    if (activeScreen == 'questions-screen') {
+      screenWidget = QuestionsScreen(
+        onSelectAnswer: chooseAnswer,
+      );
+    }
+
+    if (activeScreen == 'result-screen') {
+      screenWidget = ResultsScreen(
+        chosenAnswers: selectedAnswers,
+        onRestart: restartQuiz,
+      );
+    }
+
+    return MaterialApp(
+        home: Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(255, 81, 25, 177),
+              Color.fromARGB(255, 121, 66, 216),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: screenWidget,
+      ),
+    ));
+  }
+}
